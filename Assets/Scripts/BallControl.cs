449a0084d92323c764c44dcdea282fae3648be67
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-10-07 22:00:47 +0800

    Problem 2 Solve

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        ResetBall();
+        Invoke("PushBall", 1);
+    }
+
+
+    private void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
+    }
+
+    private void PushBall()
+    {
+        rb2d.AddForce(new Vector2(speedX, speedY));
+    }
+}
