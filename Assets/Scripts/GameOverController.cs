a5ca1f533ae42a76c39d735341d8de9137fd8b5e
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-10-08 22:19:02 +0800

    Finished

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -5,7 +5,7 @@ public class GameOverController : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.R) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
